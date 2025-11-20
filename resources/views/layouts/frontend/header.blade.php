0b6c95d97b43610808ba44992e24d27e1c919b8b
Sagar Adhikari <ssagar1916@gmail.com>
2025-11-20 13:02:06 +0545

    fix:nav active issue fix

--- resources/views/layouts/frontend/header.blade.php
+++ resources/views/layouts/frontend/header.blade.php
@@ -18,65 +18,69 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/style.css') }}">
 
     <!-- Colors -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/assets/css/colors/color1.css') }}" id="colors">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/assets/css/colors/color1.css') }}"
+        id="colors">
 
     <!-- Favicon and Touch Icons  -->
     <link rel="shortcut icon" href="{{ asset('assets/frontend/assets/icon/favicon.png') }}">
-    <link rel="apple-touch-icon-precomposed" href="{{ asset('assets/frontend/assets/icon/apple-touch-icon-158-precomposed.png') }}">
+    <link rel="apple-touch-icon-precomposed"
+        href="{{ asset('assets/frontend/assets/icon/apple-touch-icon-158-precomposed.png') }}">
 
 
 </head>
 
 <body class="header-fixed page no-sidebar header-style-2 topbar-style-2 menu-has-search">
 
-<div id="wrapper" class="animsition">
-    <div id="page" class="clearfix">
-        <!-- Header Wrap -->
-        <div id="site-header-wrap">
-            <!-- Top Bar -->
-                  <!-- Top Bar -->
-            <div id="top-bar">
-                <div id="top-bar-inner" class="container">
-                    <div class="top-bar-inner-wrap">
-                        <div class="top-bar-content">
-                            <div class="inner">
-                                <span class="address content">{{$config->address}}</span>
-                                <span class="phone content">{{$config->phone}}</span>
-                                <span class="time content">{{$config->email}}</span>
-                            </div>
-                        </div><!-- /.top-bar-content -->
+    <div id="wrapper" class="animsition">
+        <div id="page" class="clearfix">
+            <!-- Header Wrap -->
+            <div id="site-header-wrap">
+                <!-- Top Bar -->
+                <!-- Top Bar -->
+                <div id="top-bar">
+                    <div id="top-bar-inner" class="container">
+                        <div class="top-bar-inner-wrap">
+                            <div class="top-bar-content">
+                                <div class="inner">
+                                    <span class="address content">{{ $config->address }}</span>
+                                    <span class="phone content">{{ $config->phone }}</span>
+                                    <span class="time content">{{ $config->email }}</span>
+                                </div>
+                            </div><!-- /.top-bar-content -->
 
-                        <div class="top-bar-socials">
-                            <div class="inner">
-                                <span class="text">Follow us:</span>
-                                <span class="icons">
-                                    <a href="{{ $config->facebook }}"><i class="fa fa-facebook"></i></a>
-                                    <a href="{{ $config->instagram }}"><i class="fa fa-instagram"></i></a>
-                                    <a href="{{ $config->pinterest }}"><i class="fa fa-pinterest-p"></i></a>
-                                 </span>
-                            </div>
-                        </div><!-- /.top-bar-socials -->
+                            <div class="top-bar-socials">
+                                <div class="inner">
+                                    <span class="text">Follow us:</span>
+                                    <span class="icons">
+                                        <a href="{{ $config->facebook }}"><i class="fa fa-facebook"></i></a>
+                                        <a href="{{ $config->instagram }}"><i class="fa fa-instagram"></i></a>
+                                        <a href="{{ $config->pinterest }}"><i class="fa fa-pinterest-p"></i></a>
+                                    </span>
+                                </div>
+                            </div><!-- /.top-bar-socials -->
+                        </div>
                     </div>
-                </div>
-            </div><!-- /#top-bar -->
+                </div><!-- /#top-bar -->
 
-            <!-- Header -->
-            <header id="site-header">
-                <div id="site-header-inner" class="container">
-                    <div class="wrap-inner clearfix">
-                        <div id="site-logo" class="clearfix">
-                            <div id="site-log-inner">
-                                <a href="{{route('frontend.home')}}" rel="home" class="main-logo">
-                                    <img src="{{ asset($config->logo) }}" alt="logo-small" width="186" height="39" data-retina="{{ asset($config->logo) }}" data-width="186" data-height="39">
-                                </a>
-                            </div>
-                        </div><!-- /#site-logo -->
+                <!-- Header -->
+                <header id="site-header">
+                    <div id="site-header-inner" class="container">
+                        <div class="wrap-inner clearfix">
+                            <div id="site-logo" class="clearfix">
+                                <div id="site-log-inner">
+                                    <a href="{{ route('frontend.home') }}" rel="home" class="main-logo">
+                                        <img src="{{ asset($config->logo) }}" alt="logo-small" width="186"
+                                            height="39" data-retina="{{ asset($config->logo) }}" data-width="186"
+                                            data-height="39">
+                                    </a>
+                                </div>
+                            </div><!-- /#site-logo -->
 
-                        <div class="mobile-button">
-                            <span></span>
-                        </div><!-- /.mobile-button -->
+                            <div class="mobile-button">
+                                <span></span>
+                            </div><!-- /.mobile-button -->
 
-                        <nav id="main-nav" class="main-nav">
+                            {{-- <nav id="main-nav" class="main-nav">
                             <ul id="menu-primary-menu" class="menu">
                                 <li class="menu-item current-menu-item">
                                     <a href="{{route('frontend.home')}}">HOME</a>
@@ -101,9 +105,41 @@
                                     <a href="{{route('frontend.contact')}}">CONTACT</a>
                                 </li>
                             </ul>
-                        </nav>
-                        <!-- /#main-nav -->
-                    </div><!-- /.wrap-inner -->
-                </div><!-- /#site-header-inner -->
-            </header><!-- /#site-header -->
-        </div>
+                        </nav> --}}
+                            <nav id="main-nav" class="main-nav">
+                                <ul id="menu-primary-menu" class="menu">
+                                    <li
+                                        class="menu-item {{ request()->routeIs('frontend.home') ? 'current-menu-item' : '' }}">
+                                        <a href="{{ route('frontend.home') }}">HOME</a>
+                                    </li>
+                                    <li
+                                        class="menu-item {{ request()->routeIs('frontend.portfolios') ? 'current-menu-item' : '' }}">
+                                        <a href="{{ route('frontend.portfolios') }}">PORTFOLIO</a>
+                                    </li>
+                                    <li
+                                        class="menu-item {{ request()->routeIs('frontend.about') ? 'current-menu-item' : '' }}">
+                                        <a href="{{ route('frontend.about') }}">ABOUT US</a>
+                                    </li>
+                                    <li
+                                        class="menu-item {{ request()->routeIs('frontend.services') ? 'current-menu-item' : '' }}">
+                                        <a href="{{ route('frontend.services') }}">OUR SERVICES</a>
+                                    </li>
+                                    <li
+                                        class="menu-item {{ request()->routeIs('frontend.blogs') ? 'current-menu-item' : '' }}">
+                                        <a href="{{ route('frontend.blogs') }}">BLOG</a>
+                                    </li>
+                                    <li
+                                        class="menu-item {{ request()->routeIs('frontend.gallery') ? 'current-menu-item' : '' }}">
+                                        <a href="{{ route('frontend.gallery') }}">GALLERY</a>
+                                    </li>
+                                    <li
+                                        class="menu-item {{ request()->routeIs('frontend.contact') ? 'current-menu-item' : '' }}">
+                                        <a href="{{ route('frontend.contact') }}">CONTACT</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                            <!-- /#main-nav -->
+                        </div><!-- /.wrap-inner -->
+                    </div><!-- /#site-header-inner -->
+                </header><!-- /#site-header -->
+            </div>
